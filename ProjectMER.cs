465feac98b28176fbe257da878762be108c0bfe3
ScoutTrooper2 <155900485+ScoutTrooper2@users.noreply.github.com>
2025-06-01 13:27:02 +0300

    Update ProjectMER.cs

--- ProjectMER.cs
+++ ProjectMER.cs
@@ -1,5 +1,6 @@
 global using Logger = LabApi.Features.Console.Logger;
 using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
 using HarmonyLib;
 using LabApi.Events.CustomHandlers;
 using LabApi.Loader.Features.Paths;
@@ -12,29 +13,29 @@ namespace ProjectMER;
 
 public class ProjectMER : Exiled.API.Features.Plugin<Config>
 {
-	public static ProjectMER Singleton { get; private set; }
-	public static string PluginDir { get; private set; }
-	public static string MapsDir { get; private set; }
-	public static string SchematicsDir { get; private set; }
+	 public static ProjectMER Singleton { get; private set; }
+         public static string PluginDir { get; private set; }
+	 public static string MapsDir { get; private set; }
+	 public static string SchematicsDir { get; private set; }
 
-	public GenericEventsHandler GenericEventsHandler { get; } = new();
+	 public GenericEventsHandler GenericEventsHandler { get; } = new();
 
-	public ToolGunEventsHandler ToolGunEventsHandler { get; } = new();
+	 public ToolGunEventsHandler ToolGunEventsHandler { get; } = new();
 
-	public MapOnEventHandlers MapOnEventHandlers { get; } = new();
+	 public MapOnEventHandlers MapOnEventHandlers { get; } = new();
 
-	public PickupEventsHandler PickupEventsHandler { get; } = new();
+	 public PickupEventsHandler PickupEventsHandler { get; } = new();
 
-    public InvisibleTeleportEventsHandler TeleportEventsHandler { get; } = new();
+         public InvisibleTeleportEventsHandler TeleportEventsHandler { get; } = new();
 
-    public static Harmony? harmony;
-    public const string HarmonyId = "PMER-Harmony";
+         public static Harmony? harmony;
+         public const string HarmonyId = "PMER-Harmony";
 
-    public override void OnEnabled()
+         public override void OnEnabled()
 	{
 		Singleton = this;
 
-		PluginDir = Path.Combine(Paths.Configs, "MapEditorReborn");
+		PluginDir = Path.Combine(Paths.Configs, Singleton.Config.FolderName);
 		MapsDir = Path.Combine(PluginDir, "Maps");
 		SchematicsDir = Path.Combine(PluginDir, "Schematics");
 
@@ -56,35 +57,40 @@ public class ProjectMER : Exiled.API.Features.Plugin<Config>
 			Directory.CreateDirectory(SchematicsDir);
 		}
 
+		Exiled.Events.Handlers.Player.Verified += OnVerified;
+		
 		CustomHandlersManager.RegisterEventsHandler(GenericEventsHandler);
 		CustomHandlersManager.RegisterEventsHandler(ToolGunEventsHandler);
 		CustomHandlersManager.RegisterEventsHandler(MapOnEventHandlers);
 		CustomHandlersManager.RegisterEventsHandler(PickupEventsHandler);
-        CustomHandlersManager.RegisterEventsHandler(TeleportEventsHandler);
+                CustomHandlersManager.RegisterEventsHandler(TeleportEventsHandler);
 
 
-        harmony = new Harmony(HarmonyId);
+                harmony = new Harmony(HarmonyId);
 		harmony.PatchAll();
-    }
+	}
 
 	public override void OnDisabled()
 	{
 		Singleton = null!;
-
+		
 		CustomHandlersManager.UnregisterEventsHandler(GenericEventsHandler);
 		CustomHandlersManager.UnregisterEventsHandler(ToolGunEventsHandler);
 		CustomHandlersManager.UnregisterEventsHandler(MapOnEventHandlers);
 		CustomHandlersManager.UnregisterEventsHandler(PickupEventsHandler);
-        CustomHandlersManager.UnregisterEventsHandler(TeleportEventsHandler);
+                CustomHandlersManager.UnregisterEventsHandler(TeleportEventsHandler);
 
-		
-		harmony.UnpatchAll();
-        harmony = null;
-    }
+                Exiled.Events.Handlers.Player.Verified -= OnVerified;
+
+                harmony.UnpatchAll();
+                harmony = null;
+	}
 
 	public override string Name => "ProjectMER";
 
-	public override string Author => "Michal78900";
+	public override string Author => "Michal78900 (Exiled Port by ScoutTrooper)";
 
-	public override Version Version => new Version(2025, 5, 28, 1);
+	public override Version Version => new Version(2025, 6, 1, 13);
+
+	void OnVerified(VerifiedEventArgs ev) => ev.Player.SendConsoleMessage("This server using ProjectMER EXILED port. Author of port - ScoutTrooper. Original Plugin - Michal78900", "red");
 }
